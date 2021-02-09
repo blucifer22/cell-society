624c8e87805e942094273ee844a72fb181942270
joshu <joshuapetitma@yahoo.com>
2021-02-09 06:48:27 -0500

    feat(CellState): Adds typing and associated methods

--- src/main/java/cellsociety/simulation/CellState.java
+++ src/main/java/cellsociety/simulation/CellState.java
@@ -1,4 +1,17 @@
 package cellsociety.simulation;
 
-public abstract class CellState {
+public abstract class CellState<T> {
+	protected T state;
+
+	public CellState(T state) {
+		this.state = state;
+	}
+
+	public T getState() {
+		return state;
+	}
+
+	public void setState(T state) {
+		this.state = state;
+	}
 }
