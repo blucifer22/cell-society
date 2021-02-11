5efd525ac959c9551cd41e3dbd73a9c6b05edf8c
Joshua Petitma <joshua.petitma@duke.edu>
2021-02-11 16:10:19 -0500

    Removes abstract keyword from firecell
    this was an accident lol

--- src/main/java/cellsociety/simulation/FireCell.java
+++ src/main/java/cellsociety/simulation/FireCell.java
@@ -2,17 +2,37 @@ package cellsociety.simulation;
 
 import java.util.List;
 
-public abstract class FireCell extends Cell<FireState> {
+public class FireCell extends Cell<FireState> {
   protected List<Cell> neighbors;
 
-  // All FireCells need to use the same rule ∴ it's static
+  /** The rule all firecells follow to change their states.
+   *
+   * This value is set by the {@link cellsociety.simulation.Simulation}
+   * */
   public static FireRule rule;
 
+  /**
+   * Construct this cell with its default state.
+   *
+   * <p>The default state for FireCells is NORMAL.
+   */
+  public FireCell() {
+    super(new FireState(FireState.STATE.NORMAL));
+  }
+
+  /**
+   * Constructs this cell with the specified default state.
+   *
+   * @param state - The state to use for this cell.
+   */
   public FireCell(FireState state) {
-	  super(state);
+    super(state);
   }
 
-  public void computeNextState() {
-
-  }
+  /**
+   * Computes the next state for this cell.
+   *
+   * <p>Reference its current state and the rules for Fire simulations to se its next state.
+   */
+  public void computeNextState() {}
 }
