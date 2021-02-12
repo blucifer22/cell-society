4e7855dad706d16ecec6172e0d7acd81a0ceba3a
joshu <joshuapetitma@yahoo.com>
2021-02-12 07:41:45 -0500

    Adds file selection

--- src/main/java/cellsociety/Main.java
+++ src/main/java/cellsociety/Main.java
@@ -2,23 +2,28 @@ package cellsociety;
 
 import cellsociety.simulation.Simulation;
 import cellsociety.simulation.SimulationManager;
+import java.io.File;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-  private SimulationManager manager;
+  private SimulationManager simulationManager;
   public static final int SCENE_WIDTH = 500;
   public static final int SCENE_HEIGHT = 500;
   private Simulation simulation;
   private SimulationDisplay simDisplay;
   private Scene scene;
+  private Stage primaryStage;
 
   public static void main(String[] args) {
     launch(args);
@@ -26,7 +31,8 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    manager = new SimulationManager();
+    this.primaryStage = primaryStage;
+    simulationManager = new SimulationManager();
     simDisplay = new SimulationDisplay();
     BorderPane pane = new BorderPane();
     pane.setCenter(simDisplay);
@@ -53,7 +59,30 @@ public class Main extends Application {
     return row;
   }
 
+  private void newSimulation(File file) {
+    Simulation simulation = simulationManager.createSimulation(file);
+    if (simulation != null) {
+      simDisplay.setSimulation(simulation);
+    } else {
+		error("Invalid XML File");
+    }
+  }
+
+  private void error(String error) {
+    Alert a = new Alert(AlertType.ERROR, error);
+    a.show();
+  }
+
   private void loadSimulation() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Select Simulation");
+    fileChooser
+        .getExtensionFilters()
+        .addAll(new ExtensionFilter("XML Files", "*.xml"), new ExtensionFilter("All Files", ".*"));
+    File selectedFile = fileChooser.showOpenDialog(primaryStage);
+    if (selectedFile != null) {
+      newSimulation(selectedFile);
+    }
   }
 
   private void playSimulation() {}
