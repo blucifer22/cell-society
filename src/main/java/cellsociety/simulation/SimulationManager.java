a6cd9b81692236357192ec24848af80df7ea555e
joshu <joshuapetitma@yahoo.com>
2021-02-12 08:07:32 -0500

    Refactors simulation manager to use XML parser method

--- src/main/java/cellsociety/simulation/SimulationManager.java
+++ src/main/java/cellsociety/simulation/SimulationManager.java
@@ -5,18 +5,28 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The controller class that creates the simulation model for the view to use.
+ *
+ * <p>This class contains an XML parser and can create {@link cellsociety.simulation.Simulation}
+ * objects.
+ */
 public class SimulationManager {
   private XMLParser parser;
 
+  public SimulationManager() {
+    parser = new XMLParser();
+  }
+
   public Simulation createSimulation(File file) {
-	  try {
-		  XMLParser parser = new XMLParser(file);
-		  Map<String, String> metaData = parser.getSimulationMetadata();
-		  Map<String, Double> config = parser.getSimulationParameters();
-		  List<int[]> nonDefaultStates = parser.getInitialNonDefaultStates();
-		  return new Simulation(metaData, config, nonDefaultStates);
-	  } catch(Exception e) {
-		  return null;
-	  }
+    try {
+      parser.createConfiguration(file);
+      Map<String, String> metaData = parser.getSimulationMetadata();
+      Map<String, Double> config = parser.getSimulationParameters();
+      List<int[]> nonDefaultStates = parser.getInitialNonDefaultStates();
+      return new Simulation(metaData, config, nonDefaultStates);
+    } catch (Exception e) {
+      return null;
+    }
   }
 }
