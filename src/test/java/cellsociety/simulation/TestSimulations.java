d3a6e026db94fd2086541483318882f6e3c87b9a
joshu <joshuapetitma@yahoo.com>
2021-02-12 09:38:32 -0500

    Adds simple test for fire simulation

--- src/test/java/cellsociety/simulation/TestSimulations.java
+++ src/test/java/cellsociety/simulation/TestSimulations.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import cellsociety.util.XMLParser;
 import java.io.File;
+import java.util.List;
 import cellsociety.simulation.SimulationManager;
 import cellsociety.simulation.Simulation;
 
@@ -15,5 +16,13 @@ class TestSimulation {
     File file = new File("data/fireSimulation.xml");
 	SimulationManager sm = new SimulationManager();
 	Simulation sim = sm.createSimulation(file);
+	List<Cell> cells = sim.getCells();
+	boolean oneBurning = false;
+	for (Cell cell : cells) {
+		if (cell.getCurrentState().toString().equals("BURNING")) {
+			oneBurning = true;
+		}
+	}
+	assertTrue(oneBurning);
   }
 }
