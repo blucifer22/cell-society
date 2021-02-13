fe913814b77f8df4d31aaa5389c2c6f04557e93c
David Coffman <david.coffman@duke.edu>
2021-02-13 11:34:29 -0500

    Re-implement GraphicalCellGrid

---
+++ src/main/java/cellsociety/graphics/GraphicalCellRectangularGrid.java
@@ -0,0 +1,49 @@
+package cellsociety.graphics;
+
+import cellsociety.simulation.Cell;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.paint.Paint;
+
+public class GraphicalCellRectangularGrid {
+  private List<GraphicalCell> graphicalCells;
+  private Map<Integer, Paint> paintMap;
+  private Group root;
+
+  public GraphicalCellRectangularGrid(List<Cell> cells, double width, double height,
+      int numRows, int numCols) {
+    this.root = new Group();
+    this.graphicalCells = new ArrayList<>();
+    assert cells.size() == numRows*numCols;
+
+    double cellWidth = width/numCols;
+    double cellHeight = height/numRows;
+
+    for(int i = 0; i < numRows; i++) {
+      for(int j = 0; j < numCols; j++) {
+        Cell c = cells.get(i*numCols+j);
+        GraphicalCell gc = new GraphicalCell(c, paintMap, j*width, i*height, cellWidth,
+            cellHeight);
+        this.graphicalCells.add(gc);
+        renderNode(gc.getNode());
+      }
+    }
+  }
+
+  public void update() {
+    for(GraphicalCell c: graphicalCells) {
+      c.update();
+    }
+  }
+
+  private void renderNode(Node n) {
+    root.getChildren().add(n);
+  }
+
+  public Node getNode() {
+    return this.root;
+  }
+}
