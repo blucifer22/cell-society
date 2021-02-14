c035c15546045a46c3f03c4e322a3178bd4f02e2
joshu <joshuapetitma@yahoo.com>
2021-02-14 06:49:17 -0500

    Removes print statement from ConwayCell

--- src/main/java/cellsociety/simulation/ConwayCell.java
+++ src/main/java/cellsociety/simulation/ConwayCell.java
@@ -41,7 +41,6 @@ public class ConwayCell extends Cell {
    */
   public void computeNextCellState() {
     int numLiveNeighbors = 0;
-    System.out.println("Neighbors: " + neighbors.size());
     for (Cell cell : neighbors) {
       if (cell.getCurrentCellState() == ALIVE) {
         numLiveNeighbors++;
