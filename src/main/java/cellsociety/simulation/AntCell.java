7b552ea588575af46a4d89b31d6dbf7e3348bdf2
Marc Chmielewski <msc68@duke.edu>
2021-02-18 11:45:38 -0500

    Added pheromone handling behavior

--- src/main/java/cellsociety/simulation/AntCell.java
+++ src/main/java/cellsociety/simulation/AntCell.java
@@ -13,22 +13,26 @@ public class AntCell extends Cell{
   public static final int ANT = 1;
   public static final int FOOD = 2;
   public static final int HOME = 3;
+  public static final int OBSTACLE = 4;
 
   public static final String HOME_PHEROMONE_LEVEL = "HomePheromoneLevel";
   public static final String FOOD_PHEROMONE_LEVEL = "FoodPheromoneLevel";
+  public static final String PHEROMONE_EVAPORATION_RATE = "PheromoneEvaporationRate";
 
-  private double homePheromoneLevel = 0;
-  private double foodPheromoneLevel = 0;
+  private double homePheromoneLevel;
+  private double foodPheromoneLevel;
+  private double pheromoneEvaporationRate;
 
   /**
    * Construct this cell with its default state.
    *
-   * <p>The default state for WatorCells is WATER.
+   * <p>The default state for AntCells is EMPTY
    */
   public AntCell(Map<String, Double> rules) {
     super(EMPTY, rules);
     homePheromoneLevel = 0;
     foodPheromoneLevel = 0;
+    pheromoneEvaporationRate = rules.get(PHEROMONE_EVAPORATION_RATE);
   }
 
   /**
@@ -39,7 +43,23 @@ public class AntCell extends Cell{
    *
    */
   public void computeNextCellState() {
+    switch(cellState) {
+      case EMPTY -> {
+        updatePheromoneLevels(this);
+      }
+      case ANT -> {
+        // TODO: Implement ANT cell behavior
+      }
+    }
+  }
 
+  private void updatePheromoneLevels(AntCell cell) {
+    if(cell.homePheromoneLevel > 0) {
+      cell.homePheromoneLevel = Math.max(0, cell.homePheromoneLevel- cell.pheromoneEvaporationRate);
+    }
+    if(cell.foodPheromoneLevel > 0) {
+      cell.foodPheromoneLevel = Math.max(0, cell.foodPheromoneLevel- cell.pheromoneEvaporationRate);
+    }
   }
 
 }
