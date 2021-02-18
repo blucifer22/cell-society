7e3545612233d65645872ee9688ef50b2a1e22ce
Marc Chmielewski <msc68@duke.edu>
2021-02-18 18:27:08 -0500

    Completed AntCell logic

--- src/main/java/cellsociety/simulation/AntCell.java
+++ src/main/java/cellsociety/simulation/AntCell.java
@@ -1,6 +1,5 @@
 package cellsociety.simulation;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
@@ -19,8 +18,6 @@ public class AntCell extends Cell{
   public static final int HOME = 3;
   public static final int OBSTACLE = 4;
 
-  private static final String HOME_PHEROMONE_CONCENTRATION = "HomePheromoneConcentration";
-  private static final String FOOD_PHEROMONE_CONCENTRATION = "FoodPheromoneConcentration";
   private static final String TARGET_PHEROMONE_CONCENTRATION = "TargetPheromoneConcentration";
   private static final String PHEROMONE_EVAPORATION_RATE = "PheromoneEvaporationRate";
   private static final String HAS_FOOD = "HasFood";
@@ -99,7 +96,6 @@ public class AntCell extends Cell{
   }
 
   private void move(AntCell move) {
-    this.nextCellState = EMPTY;
     if(this.hasFood == 1.0) {
       this.foodPheromoneConcentration = targetPheromoneConcentration;
     }
@@ -107,6 +103,9 @@ public class AntCell extends Cell{
       this.homePheromoneConcentration = targetPheromoneConcentration;
     }
     move.nextCellState = ANT;
+    move.hasFood = this.hasFood;
+    this.nextCellState = EMPTY;
+    this.hasFood = 0.0;
   }
 
   private AntCell checkHomeMove(Set<AntCell> availableNeighbors) {
@@ -151,6 +150,25 @@ public class AntCell extends Cell{
     }
   }
 
+  private AntCell checkFoodMove(Set<AntCell> availableNeighbors) {
+    double maxPheromoneConcentration = 0.0;
+    AntCell moveCell = null;
+    for(AntCell cell : availableNeighbors) {
+      if(cell.cellState == FOOD) { // If FOOD is in range?
+        return cell;
+      }
+      else if(cell.foodPheromoneConcentration > maxPheromoneConcentration) { // Most pheromones?
+        moveCell = cell;
+        maxPheromoneConcentration = cell.foodPheromoneConcentration;
+      }
+    }
+    if(moveCell == null) { // If none of the above, settle for random...
+      int index = rng.nextInt(availableNeighbors.size());
+      moveCell = getRandomNeighbor(availableNeighbors, index);
+    }
+    return moveCell;
+  }
+
   private Set<AntCell> findAvailableNeighbors() {
     Set<AntCell> availableNeighbors = new HashSet<>();
     for(Cell cell : neighbors) {
@@ -171,5 +189,4 @@ public class AntCell extends Cell{
           Math.max(0, this.foodPheromoneConcentration - this.pheromoneEvaporationRate);
     }
   }
-
 }
