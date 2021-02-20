5e416f7ee6bb8894c9d7ae8c933c52d78aa6f68f
Marc Chmielewski <msc68@duke.edu>
2021-02-19 21:13:34 -0500

    Regrow and consume

--- src/main/java/cellsociety/simulation/SugarCell.java
+++ src/main/java/cellsociety/simulation/SugarCell.java
@@ -1,8 +1,9 @@
 package cellsociety.simulation;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * This class handles the behavior of Cells in the SugarScape simulation, and thus the state
@@ -15,12 +16,13 @@ public class SugarCell extends Cell {
   public static final int AGENT = 1;
 
   private static final String MAX_SUGAR_CAPACITY = "MaxSugarCapacity";
-  private static final String SUGAR_GROW_BACK_RATE = "SugarGrowBackRate";
+  private static final String SUGAR_REGROWTH_RATE = "SugarRegrowthRate";
   private static final String SUGAR_METABOLISM_RATE = "SugarMetabolismRate";
+  private static final String AGENT_SUGAR = "AgentSugar";
 
   private double agentSugar;
   private double patchSugar;
-  private final double sugarGrowBackRate;
+  private final double sugarRegrowthRate;
   private final double sugarMetabolismRate;
   private Random rng;
 
@@ -32,7 +34,7 @@ public class SugarCell extends Cell {
   public SugarCell(Map<String, Double> params) {
     super(PATCH, params);
     patchSugar = getParam(MAX_SUGAR_CAPACITY);
-    sugarGrowBackRate = getParam(SUGAR_GROW_BACK_RATE);
+    sugarRegrowthRate = getParam(SUGAR_REGROWTH_RATE);
     sugarMetabolismRate = getParam(SUGAR_METABOLISM_RATE);
     rng = new Random();
   }
@@ -43,23 +45,58 @@ public class SugarCell extends Cell {
     if (state == PATCH) {
       patchSugar = getParam(MAX_SUGAR_CAPACITY);
     } else if (state == AGENT) {
-      agentSugar = rng.nextInt((int) getParam(MAX_SUGAR_CAPACITY));
+      agentSugar = rng.nextInt((int) getParam(MAX_SUGAR_CAPACITY)); // Start the agents with random sugar
     }
   }
 
   @Override
   protected void setNextCellState(int state, Map<String, Double> values) {
     super.setNextCellState(state);
-    if(state == PATCH) {
-
-    }
-    else if(state == AGENT) {
-
+    if(state == AGENT) {
+      agentSugar = values.getOrDefault(AGENT_SUGAR, getParam(MAX_SUGAR_CAPACITY));
     }
   }
 
+  /**
+   * Computes the next state of this Cell by inspecting its neighbors and then determining the
+   * transition accordingly.
+   *
+   * <p>SugarScape Rules are as follows:
+   *
+   */
   public void computeNextCellState() {
+    Set<SugarCell> availableNeighbors = findAvailableNeighbors();
 
+    switch(cellState) {
+      case PATCH -> regrowSugar();
+      case AGENT -> {
+        findSugar(availableNeighbors);
+        consumeSugar();
+      }
+    }
   }
 
+  private void findSugar(Set<SugarCell> availableNeighbors) {
+  }
+
+  private void regrowSugar() {
+    this.patchSugar = Math.min(this.patchSugar + sugarRegrowthRate, getParam(MAX_SUGAR_CAPACITY));
+  }
+
+  private void consumeSugar() {
+    this.agentSugar = Math.max(0, agentSugar - sugarMetabolismRate);
+    if(this.agentSugar <= 0) {
+      this.nextCellState = PATCH;
+    }
+  }
+
+  private Set<SugarCell> findAvailableNeighbors() {
+    Set<SugarCell> availableNeighbors = new HashSet<>();
+    for(Cell cell : neighbors) {
+      if((cell.getCurrentCellState() != AGENT && cell.getNextCellState() != AGENT) {
+        availableNeighbors.add((SugarCell)cell);
+      }
+    }
+    return availableNeighbors;
+  }
 }
