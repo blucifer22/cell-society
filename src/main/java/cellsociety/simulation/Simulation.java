284a8bf70c579e6037475faab98a8c7931b401c5
David Coffman <david.coffman@duke.edu>
2021-02-20 21:36:30 -0500

    Fix bug

--- src/main/java/cellsociety/simulation/Simulation.java
+++ src/main/java/cellsociety/simulation/Simulation.java
@@ -35,7 +35,7 @@ public class Simulation {
   protected void initialize(List<Cell> cells) {
     this.cells = cells;
     this.cellGrid = new CellGrid(cells, configuration.getWidth(), configuration.getHeight(),
-        configuration.getCellShape(), configuration.getGridType());
+        configuration.getCellShape(), configuration.getEdgeType());
     for (int[] arr : nonDefaultStates) {
       Cell cell = cellGrid.getCell(arr[0], arr[1]);
       cell.setCellState(arr[2]);
