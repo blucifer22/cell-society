54058e145836391bbe53f07dd1a6a11281553ab6
joshu <joshuapetitma@yahoo.com>
2021-02-21 19:10:16 -0500

    Adds test for medium sized grids

--- src/test/java/cellsociety/simulation/GridTests.java
+++ src/test/java/cellsociety/simulation/GridTests.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 // Name doesn't really matter
 class TestGrids {
 
-  @DisplayName("If the neighborhoods are full then there should be 8 rectangle neighbors")
+  @DisplayName("If the neighborhoods are large then there should be 8 rectangle neighbors")
   @Test
   void testFullNeighborhoods() {
     File file = new File("data/fire/absolute_fire_test.xml");
@@ -28,6 +28,25 @@ class TestGrids {
 
     List<Cell> cells = sim.getCells();
     long onFire = cells.stream().filter(c -> c.getCurrentCellState() == BURNING).count();
-    assertEquals(onFire, 8);
+    assertEquals(8, onFire);
+  }
+
+  @DisplayName("If the neighborhoods are medium then there should be 4 rectangle neighbors")
+  @Test
+  void testMediumNeighborhoods() {
+    File file = new File("data/fire/absolute_medium_fire_test.xml");
+    SimulationFactory fac = new SimulationFactory();
+    try {
+      fac.loadSimulationFile(file);
+    } catch (Exception e) {
+      e.printStackTrace();
+      assertTrue(false); // Lol why not just throw a failure? Cuz that makes no sense.
+    }
+    Simulation sim = fac.getSimulation();
+    sim.step();
+
+    List<Cell> cells = sim.getCells();
+    long onFire = cells.stream().filter(c -> c.getCurrentCellState() == BURNING).count();
+    assertEquals(4, onFire);
   }
 }
