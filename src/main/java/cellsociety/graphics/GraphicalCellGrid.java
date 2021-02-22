1d0943917b972d8bdd68e99ddd41e5317ebea861
David Coffman <david.coffman@duke.edu>
2021-02-21 22:45:00 -0500

    Write GCGrid docs

--- src/main/java/cellsociety/graphics/GraphicalCellGrid.java
+++ src/main/java/cellsociety/graphics/GraphicalCellGrid.java
@@ -12,8 +12,9 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.transform.Affine;
 
+// TODO: Finish this Javadoc!
 /**
- * A Grid that configures the visual cells into a grid pattern.
+ * A class that configures {@link GraphicalCell}s into a grid pattern.
  *
  * @author David Coffman
  */
@@ -22,6 +23,17 @@ public class GraphicalCellGrid {
   private final List<GraphicalCell> graphicalCells;
   private final Group root;
 
+  /**
+   * Sole constructor of <code>GraphicalCellGrid</code>s. Takes geometric parameters in order to
+   * lay out any of the {@link GraphicalCell} subclasses into a grid structure.
+   *
+   * @param cellShape the {@link CellShape} to use when constructing the grid
+   * @param cells the model {@link Cell}s to render
+   * @param width the pixel width of the grid
+   * @param height the pixel height of the grid
+   * @param numRows the cell height of the grid
+   * @param numCols the cell width of the grid
+   */
   public GraphicalCellGrid(CellShape cellShape, List<Cell> cells, double width, double height,
       int numRows, int numCols) {
     this.root = new Group();
@@ -53,6 +65,9 @@ public class GraphicalCellGrid {
     centerAndScaleGrid(width, height);
   }
 
+  // Centers the grid and re-scales it so it fits within the width and height dimensions. Would
+  // be critical for an infinite grid implementation, but also useful for GraphicalCell
+  // subclasses where dimensions are difficult to calculate and auto-scaling is much simpler.
   private void centerAndScaleGrid(double width, double height) {
     double widthRatio = width/root.getBoundsInParent().getWidth();
     double heightRatio = height/root.getBoundsInParent().getHeight();
@@ -69,16 +84,26 @@ public class GraphicalCellGrid {
     this.root.getTransforms().addAll(translate);
   }
 
+  /**
+   * Updates each {@link GraphicalCell} in the <code>GraphicalCellGrid</code>.
+   */
   public void update() {
     for (GraphicalCell c : graphicalCells) {
       c.update();
     }
   }
 
+  // Utility method to make the syntax for adding a Node to the scene graph cleaner
   private void renderNode(Node n) {
     root.getChildren().add(n);
   }
 
+  /**
+   * Exposes the <code>GraphicalCellGrid</code>'s rendering node so it can be rendered by a
+   * <code>Scene</code>.
+   *
+   * @return the <code>GraphicalCellGrid</code>'s rendering node
+   */
   public Node getNode() {
     return this.root;
   }
