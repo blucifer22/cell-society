aa717fcd5e5d39a736e622a3ebe5d37de9e31d68
David Coffman <david.coffman@duke.edu>
2021-02-21 22:53:47 -0500

    Write SimulationWriter javadoc

--- src/main/java/cellsociety/util/SimulationWriter.java
+++ src/main/java/cellsociety/util/SimulationWriter.java
@@ -14,11 +14,25 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+/**
+ * Class responsible for writing a {@link SimulationConfiguration} and set of {@link Cell}s back to
+ * disk after they have been (potentially) modified.
+ *
+ * @author David Coffman
+ */
 public class SimulationWriter {
   private final SimulationConfiguration config;
   private final List<Cell> cells;
   private final Document doc;
 
+  /**
+   * Constructs a DOM tree based on the input {@link SimulationConfiguration} and {@link Cell}s
+   * which can then be written to disk with a call to {@link SimulationWriter#writeToFile(File)}.
+   *
+   * @param config the {@link SimulationConfiguration} to write to disk
+   * @param cells the list of {@link Cell}s to write to disk
+   * @throws Exception if the DOM tree construction fails
+   */
   public SimulationWriter(SimulationConfiguration config, List<Cell> cells) throws Exception {
     this.config = config;
     this.cells = cells;
@@ -26,6 +40,7 @@ public class SimulationWriter {
     buildDocument();
   }
 
+  // Builds the DOM tree
   private void buildDocument() {
     Node root = doc.createElement("xml");
     doc.appendChild(root);
@@ -36,6 +51,7 @@ public class SimulationWriter {
     buildInitialStates(root);
   }
 
+  // Builds the <General> tree based on the SimulationConfiguration
   private void buildMetadata(Node root) {
     Node generalElement = doc.createElement("General");
     root.appendChild(generalElement);
@@ -60,6 +76,7 @@ public class SimulationWriter {
     }
   }
 
+  // Builds the <GeometricConfiguration> tree based on the SimulationConfiguration
   private void buildGeometricConfiguration(Node root) {
     Node geometricConfigElement = doc.createElement("GeometricConfiguration");
     root.appendChild(geometricConfigElement);
@@ -86,6 +103,7 @@ public class SimulationWriter {
     }
   }
 
+  // Builds the <SimulationParameters> tree based on the SimulationConfiguration's parameter map
   private void buildSimulationParameters(Node root) {
     Node simulationParamElement = doc.createElement("SimulationParameters");
     root.appendChild(simulationParamElement);
@@ -98,6 +116,7 @@ public class SimulationWriter {
     }
   }
 
+  // Builds the <InitialStates> tree based on the list of cells given as a constructor parameter
   private void buildInitialStates(Node root) {
     Node initialStateElement = doc.createElement("InitialStates");
     root.appendChild(initialStateElement);
@@ -126,10 +145,14 @@ public class SimulationWriter {
     }
   }
 
+  /**
+   * Writes the DOM tree to disk after it is constructed (by the object's constructor).
+   *
+   * @param f the {@link File} to which the {@link SimulationConfiguration}'s contents should be
+   *          written
+   * @throws Exception if the write fails
+   */
   public void writeToFile(File f) throws Exception {
-//    if(!f.createNewFile()) {
-//      throw new FileAlreadyExistsException("A file with that name already exists.");
-//    }
     // Adapted from tutorial at https://www.baeldung.com/java-write-xml-document-file.
     f.createNewFile();
     BufferedWriter w = new BufferedWriter(new FileWriter(f));
